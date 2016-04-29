b3f688321fddb90ed3c84b9dcbb8d6561bc9aa8e
douglas alvares virgolino <dougpc93@gmail.com>
2016-04-29 13:28:32 -0300

    1.0

--- AtividadeUnama/src/main/java/com/unama/atividadeunama/model/Produto.java
+++ AtividadeUnama/src/main/java/com/unama/atividadeunama/model/Produto.java
@@ -5,10 +5,23 @@
  */
 package com.unama.atividadeunama.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author douglas
  */
+@Entity
 public class Produto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    
+    private String nome;
+    
+    private String descricao;
 }
